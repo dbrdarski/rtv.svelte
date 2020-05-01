af0141eb03d5f5fa1df9606cc25274707e18f943
Dane Brdarski <dane.brdarski@gmail.com>
2020-05-01 09:25:06 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import Youtube from "@sveltecasts/svelte-youtube";
+let name = 'world';
+</script>
+
+<div class="video-screen">
+	<Youtube videoId="Ank3IdQHOuE" />
+</div>
+
+<style>
+:global(body) {
+	margin: 0;
+	padding: 0;
+}
+</style>
